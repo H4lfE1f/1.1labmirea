7d71e5491a5ccc52695f3dacb59618bc962fc574
unknown <ivcbibl@VLAN-25-BI-001.rd.mirea.ru>
2021-02-09 19:02:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import math
+from math import *
+
+x = 77
+y = 48
+f = (x**5 + x**3) / (40 * y**5 - tan(y)) + sqrt((sin(y) + y**8 - 63) / (28 * x**6 + 89 * x**5)) - (x - sin(y))
+print ("{:e}".format(f))
