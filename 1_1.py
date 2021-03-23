2ef6d8d872a98a553a00377f6f6aa431654cfc12
H4lfE1f <70902074+H4lfE1f@users.noreply.github.com>
2021-03-23 17:32:49 +0300

    1.1tobot

---
+++ 1_1.py
@@ -0,0 +1,9 @@
+import math
+from math import *
+def f11 (x, y):
+    f = (x ** 5 + x ** 3) / (40 * y ** 5 - tan(y)) + sqrt((sin(y) + y ** 8 - 63) / (28 * x ** 6 + 89 * x ** 5)) - (x - sin(y))
+    return f
+# print (f11(77, 48))
+
+
+
